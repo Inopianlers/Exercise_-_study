de72a2ad64a075d73ff7c66e737173d6e65a411b
Inopianlers <101866570+Inopianlers@users.noreply.github.com>
2022-06-20 09:45:43 +0800

    Add files via upload

---
+++ EXERcise/myscripts.js
@@ -0,0 +1,56 @@
+// function printArray(arr) {
+//     for (var i=0;i<arr.length;i++){
+//       for(var j=0;j<arr[0].length;j++){
+//         console.log(arr[i][j]);
+//       }   
+//     }
+//   }
+//   var arr = [[1, 2],
+//             [3, 4],
+//             [5, 6]];
+  
+
+
+
+
+
+//  while(condition) {
+         // Code to be executed     
+//           }   
+          
+var i = 1;
+while(i <= 5) {    
+        document.write("<p>The number is " + i + "</p>");
+        i++;
+          }
+
+
+
+
+
+
+        //   do {
+            // Code to be executed
+        // }
+        // while(condition);
+
+var i = 1;
+    do {
+    document.write("<p>The number is " + i + "</p>");
+    i++;
+    }
+      while(i <= 5);
+
+
+
+
+
+
+
+    //   for(initialization; condition; increment) {
+       // Code to be executed 
+    // }
+
+    for(var i=1; i<=5; i++) {
+      document.write("<p>The number is " + i + "</p>");
+  }
